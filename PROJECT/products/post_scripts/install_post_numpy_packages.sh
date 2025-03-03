f6294bd9829eb53cb7881acdb1906b435e74ddc4
Gregor Simič <simic.gregor@gmail.com>
2025-03-03 23:31:59 +0100

    Include meshio==5.3.5 in numpy post install script

--- PROJECT/products/post_scripts/install_post_numpy_packages.sh
+++ PROJECT/products/post_scripts/install_post_numpy_packages.sh
@@ -7,7 +7,7 @@ PATH=${PYTHON_ROOT_DIR}/bin LD_LIBRARY_PATH=${PYTHON_ROOT_DIR}/lib python -m pip
 PACKAGES="pybind11 pythran ninja" # Scipy
 PACKAGES+=" cycler python-dateutil kiwisolver pyparsing six fonttools pytz contourpy" # Matplotlib
 PACKAGES+=" Sphinx sphinxcontrib_websupport sphinx_rtd_theme sphinx-intl Pygments markupsafe" # Sphinx
-PACKAGES+=" Pillow" # Miscellaneous
+PACKAGES+=" Pillow meshio==5.3.5" # Miscellaneous
 
 echo " pip install ${PACKAGES}"
 PATH=${PYTHON_ROOT_DIR}/bin LD_LIBRARY_PATH=${PYTHON_ROOT_DIR}/lib python -m pip install ${PACKAGES}
