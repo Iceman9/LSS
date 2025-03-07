626ddecf09c3433ba8f752c17f8b16704565c83f
Gregor Simič <simic.gregor@gmail.com>
2025-03-07 10:59:51 +0100

    Fix formatting in env_script

--- PROJECT/products/env_scripts/scipy.py
+++ PROJECT/products/env_scripts/scipy.py
@@ -2,11 +2,11 @@ import os
 import platform
 
 def set_env(env: 'Environ', prereq_dir: str, version: str) -> None:
-  env.set("SCIPY_ROOT_DIR", prereq_dir)
-  if not platform.system() == "Windows" :
-      pyver = 'python' + env.get('PYTHON_VERSION')
-      env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver,
-                                             'site-packages'))
+    env.set("SCIPY_ROOT_DIR", prereq_dir)
+    if not platform.system() == "Windows" :
+        pyver = 'python' + env.get('PYTHON_VERSION')
+        env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver,
+                                               'site-packages'))
 
 def set_nativ_env(env: 'Environ') -> None:
     pass
